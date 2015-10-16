e2b85dd91719f620bf66a038be79c3d7e58f2578
Greg Skiano <greg.skiano@dowjones.com>
2015-10-16 14:53:03 -0400

    making dynamic component names work with state free components

--- example/examples/Chess.js
+++ example/examples/Chess.js
@@ -1,48 +1,35 @@
 
-import React, {PropTypes} from 'react';
+import React from 'react';
 import {Column, Row, observeGrid} from '../../src';
 
-const Cell = React.createClass({
-  propTypes: {
-    dark: PropTypes.bool,
-    rows: PropTypes.number
-  },
+export default function Chess () {
+  const Rows = [];
 
-  render() {
-    const style = {
-      position: 'relative',
-      height: 0,
-      paddingBottom: '100%',
-      background: this.props.dark ? '#113300' : '#ffaa00',
-      marginBottom: 20
-    };
-    return <div style={style}/>
-  }
-});
+  for (let r = 0; r < 8; r += 1) {
+    const Columns = [];
 
-export default observeGrid(React.createClass({
-  propTypes: {
-    colWidth: PropTypes.number
-  },
-
-  getDefaultProps() {
-    return {
-      rows: 5
-    };
-  },
-
-  render() {
-    const Rows = [];
-
-    for (let r = 0; r < 8; r += 1) {
-      const Columns = [];
-
-      for (let c = 0; c < 8; c += 1) {
-        Columns.push(<Column width="1/8" key={c}><Cell dark={!!((r + c) % 2)}/></Column>);
-      }
-
-      Rows.push(<Row key={r}>{Columns}</Row>);
+    for (let c = 0; c < 8; c += 1) {
+      Columns.push(
+        <Column width="1/8" key={c}>
+          <Cell dark={!!((r + c) % 2)}/>
+        </Column>
+      );
     }
-    return <div>{Rows}</div>;
+
+    Rows.push(<Row key={r}>{Columns}</Row>);
   }
-}));
+
+  return <div>{Rows}</div>;
+};
+
+function Cell({dark}) {
+  const style = {
+    position: 'relative',
+    height: 0,
+    paddingBottom: '100%',
+    background: dark ? '#113300' : '#ffaa00',
+    marginBottom: 20
+  };
+  return <div style={style}/>
+}
+
