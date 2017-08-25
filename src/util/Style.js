1ac6e2cf86c3343997fb18c971173ef0d1f1c7a2
Jorrit Schippers <jorrit@ncode.nl>
2017-08-25 21:09:21 +0200

    Remove browser prefixes for box-sizing
    `box-sizing` is supported by all browsers, even in older versions, without a prefix.

--- src/util/Style.js
+++ src/util/Style.js
@@ -8,7 +8,7 @@ const Style = (props) => {
   const style = `
     .${ctx} .${ROW}:after, .${ctx} .${COL}:after{content:'';display:block;height:0;visibility:hidden;clear:both;}
     .${ctx} .${ROW}{margin-left:auto;margin-right:auto;width:100%;}
-    .${ctx} .${COL}{box-sizing:border-box;-ms-box-sizing:border-box;-moz-box-sizing:border-box;width:100%;max-width:100%;float:left;min-height:1px;}
+    .${ctx} .${COL}{box-sizing:border-box;width:100%;max-width:100%;float:left;min-height:1px;}
     .${ctx} .${COL}{padding-left:${gutter}px;padding-right:${gutter}px;}
     .${ctx} .${COL} .${ROW}{margin-left:-${gutter}px;margin-right:-${gutter}px;width:auto;}
   `;
